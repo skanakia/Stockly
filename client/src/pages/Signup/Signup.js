3eafb29aa7464c56c708655893f1e26ee5d1b3e0
skanakia <skanakia1@gmail.com>
2018-08-14 07:58:33 -0400

    fix

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react'
 import axios from 'axios'
-import API from '../../util/API'
+import API from '../../utils/API'
 import './Signup.css'
 
 class Signup extends Component {
