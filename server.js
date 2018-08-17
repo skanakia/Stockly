aae92fab1f9fc76572cd46d8bf55f77d10ba0d4d
skanakia <skanakia1@gmail.com>
2018-08-17 09:20:50 -0400

    server fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(routes);
 
 //require the routes
