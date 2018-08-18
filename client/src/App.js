02dbcd4dce1cc65def8bf92e79eda52ddc2bd2f9
skanakia <skanakia1@gmail.com>
2018-08-18 04:03:40 -0400

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -31,9 +31,9 @@ class App extends Component {
   }
 
   getUser() {
-    axios.get('/api/user/current', { credentials : 'same-origin' }).then(response => {
+    axios.get('/api/user/current', { headers: {Accepts: "application/json"}, withCredentials: true, credentials : 'same-origin' }).then(response => {
       console.log('Get user response: ')
-      console.log(response.data.email)
+      console.log(response)
       if (response.data.email) {
         console.log('Get User: There is a user saved in the server session: ')
 
