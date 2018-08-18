3affeda6cd101b54cfa525c11bf9a4e5f9a41d62
skanakia <skanakia1@gmail.com>
2018-08-18 11:14:23 -0400

    server fix

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(routes);
 
@@ -26,12 +26,11 @@ app.use(routes);
 require("./config/passport.js")(app);
 require("./routes/api/user-api-routes.js")(app);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 
 mongoose.Promise = global.Promise
