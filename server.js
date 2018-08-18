429478c06c1a0f1c0f4139d3c202a939bebb6582
skanakia <skanakia1@gmail.com>
2018-08-18 05:17:54 -0400

    fix

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const morgan = require("morgan")
 const app = express();
 const routes = require("./routes");
 // const db = require("./models");
+const cors = require('cors');
 
+
+app.use(cors());
 
 //express.use boilerplate
 app.use(morgan('dev'));
